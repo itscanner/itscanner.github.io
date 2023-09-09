5806204dce022c5d0e535c31623319d560275b98
itscanner <nagela.connor@gmail.com>
2023-09-09 14:55:48 -0500

    change configuration again

--- next.config.js
+++ next.config.js
@@ -1,18 +1,9 @@
 /** @type {import('next').NextConfig} */
 // next.config.js
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false
-
-let assetPrefix = ''
-let basePath = '/'
-
-if (isGithubActions) {
-  // trim off `<owner>/`
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
-
-  assetPrefix = `/${repo}/`
-  basePath = `/${repo}`
-}
+const repo = 'itscanner.github.io'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
 
 module.exports = {
   assetPrefix: assetPrefix,
